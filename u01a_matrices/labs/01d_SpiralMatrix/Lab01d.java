8286eb6aabbb9fe2ce015214f91b4abc0afcd3e6
aheliChatterjee975 <caheli688@gmail.com>
2026-09-14 08:48:37 -0500

    Made more final changes

---
+++ u01a_matrices/labs/01d_SpiralMatrix/Lab01d.java
@@ -0,0 +1,16 @@
+//© A+ Computer Science  -  www.apluscompsci.com
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class Lab01d {
+
+    public static void main(String args[]) {
+        SpiralMatrix test = new SpiralMatrix(5);
+        test.createSpiral();
+        System.out.println(test.toString());
+    }
+}
