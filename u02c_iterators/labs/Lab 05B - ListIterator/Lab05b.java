be1aaff0bfa10ce856fdf9c393cbb8a708744487
aheliChatterjee975 <caheli688@gmail.com>
2026-09-15 08:27:15 -0500

    Finished the first two iterator labs

--- u02c_iterators/labs/Lab 05B - ListIterator/Lab05b.java
+++ u02c_iterators/labs/Lab 05B - ListIterator/Lab05b.java
@@ -11,8 +11,30 @@ import static java.lang.System.*;
 
 public class Lab05b
 {
-	public static void main ( String[] args )
-	{
-		//add test cases		
-	}
+   public static void main(String[] args)
+   {
+      ListIteratorTest test;
+
+      test = new ListIteratorTest("a b c d e");
+      out.println(test);
+
+      test.setTest("c");
+      out.println(test);
+
+      test.replace("b", "B");
+      out.println(test);
+
+      test.replace("d", "D");
+      out.println(test);
+
+
+      test = new ListIteratorTest("one two three two four");
+      out.println(test);
+
+      test.setTest("two");
+      out.println(test);
+
+      test.replace("three", "THREE");
+      out.println(test);
+   }
 }
--- u02c_iterators/labs/Lab 05B - ListIterator/ListIteratorTest.java
+++ u02c_iterators/labs/Lab 05B - ListIterator/ListIteratorTest.java
@@ -11,18 +11,43 @@ import static java.lang.System.*;
 
 public class ListIteratorTest
 {
-	private ArrayList<String> list;
+   private ArrayList<String> list;
 
-	public ListIteratorTest(String line) {
-	}
+   public ListIteratorTest(String line) {
+      list = new ArrayList<String>();
+      list.addAll(Arrays.asList(line.split(" ")));
+   }
 
-	public void setTest(String line) {
-	}
+   public void setTest(String line) {
+      ListIterator<String> iter = list.listIterator();
 
-	public void replace(String toRemove, String replaceWith) {
-	}
+      while(iter.hasNext()) {
+         iter.next();
+      }
 
-	public String toString() {
-		return "";
-	}
+      while(iter.hasPrevious()) {
+         String item = iter.previous();
+
+         if(item.equals(line)) {
+            iter.set(line + "*");
+         }
+      }
+   }
+
+   public void replace(String toRemove, String replaceWith) {
+      ListIterator<String> iter = list.listIterator();
+
+      while(iter.hasNext()) {
+         String item = iter.next();
+
+         if(item.equals(toRemove)) {
+            iter.set(replaceWith);
+         }
+      }
+   }
+
+   public String toString() {
+      return list.toString();
+   }
 }
+
