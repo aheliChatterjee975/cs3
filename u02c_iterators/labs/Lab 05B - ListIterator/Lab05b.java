ab1e71b52cd49b7515d5684728d2f22df4e48aee
aheliChatterjee975 <caheli688@gmail.com>
2026-09-22 09:04:09 -0500

    Did the first two labs

--- u02c_iterators/labs/Lab 05B - ListIterator/Lab05b.java
+++ u02c_iterators/labs/Lab 05B - ListIterator/Lab05b.java
@@ -13,28 +13,8 @@ public class Lab05b
 {
    public static void main(String[] args)
    {
-      ListIteratorTest test;
-
-      test = new ListIteratorTest("a b c d e");
-      out.println(test);
-
-      test.setTest("c");
-      out.println(test);
-
-      test.replace("b", "B");
-      out.println(test);
-
-      test.replace("d", "D");
-      out.println(test);
-
-
-      test = new ListIteratorTest("one two three two four");
-      out.println(test);
-
-      test.setTest("two");
-      out.println(test);
-
-      test.replace("three", "THREE");
+      ListIteratorTest test = new ListIteratorTest("a b c a b c");
+      test.replace("a", "+");
       out.println(test);
    }
 }
